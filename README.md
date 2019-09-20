06bf2296353b7a4cf1342857444c69ea80c55ea7
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2019-09-20 19:39:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Install project
+
+- npm install
+
+### Required file
+
+- Add .env file to root (see .env-example)
+
+### Bot token
+
+- Copy discord bot token to .env (see .env-example)
---
+++ app.js
@@ -0,0 +1,139 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const template = require("./templates/raid-template.json");
+const dotenv = require("dotenv").config();
+const debounce = require("debounce");
+
+const ADD = "messageReactionAdd";
+const REMOVE = "messageReactionRemove";
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", msg => {
+  if (!msg.content.startsWith(process.env.PREFIX) || msg.author.bot) {
+    msg.react(msg.guild.emojis.find(el => el.name === "tank").id);
+    msg.react(msg.guild.emojis.find(el => el.name === "dps").id);
+    msg.react(msg.guild.emojis.find(el => el.name === "healer").id);
+  } else {
+    const args = msg.content
+      .slice(process.env.PREFIX.length)
+      .trim()
+      .split(/ +/g);
+
+    if (args.includes("new")) {
+      if (args.includes("raid")) {
+        msg.channel.send(createNewEvent(msg, "raid"));
+      } else if (args.includes("m+")) {
+        msg.channel.send(createNewEvent(msg, "m+"));
+      }
+    }
+  }
+});
+
+client.on(ADD, (reaction, user) => {
+  new Promise(resolve => {
+    let temp = reaction.message.reactions.filter(
+      reaction =>
+        reaction.emoji.name === "tank" ||
+        reaction.emoji.name === "dps" ||
+        reaction.emoji.name === "healer"
+    );
+    resolve(temp);
+  })
+    .then(res => {
+      reaction.message.reactions = res;
+      return reaction;
+    })
+    .then(res => {
+      console.log(res.message.reactions);
+      editSignups(ADD, res, user);
+    });
+});
+
+client.on(REMOVE, (reaction, user) => {
+  editSignups(REMOVE, reaction, user);
+});
+
+editSignups = (type, reaction, user) => {
+  console.log(user.username);
+  if (user.username !== "Signcraft") {
+    switch (reaction.emoji.name) {
+      case "tank":
+        template.tank.list = reaction.users
+          .filter(user => user.username !== reaction.message.author.username)
+          .map(user => user.username);
+        if (type === REMOVE) {
+          if (template.tank.list.length == 0) template.tank.list = "-";
+        }
+        break;
+      case "healer":
+        template.healer.list = reaction.users
+          .filter(user => user.username !== reaction.message.author.username)
+          .map(user => user.username);
+        if (type === REMOVE) {
+          if (template.healer.list.length == 0) template.healer.list = "-";
+        }
+        break;
+      case "dps":
+        template.dps.list = reaction.users
+          .filter(user => user.username !== reaction.message.author.username)
+          .map(user => user.username);
+        if (type === REMOVE) {
+          if (template.dps.list.length == 0) template.dps.list = "-";
+        }
+        break;
+    }
+    debounce(reaction.message.edit(createNewEvent(reaction.message)), 500);
+  } else {
+    debounce(reaction.message.edit(createNewEvent(reaction.message)), 500);
+  }
+};
+
+createNewEvent = msg => {
+  return new Discord.RichEmbed()
+    .setAuthor(msg.guild.name)
+    .setTitle(template.title)
+    .setColor(0x00ae86)
+    .setDescription(template.description)
+    .setThumbnail(msg.guild.iconURL)
+    .setFooter("Moister than an oyster..")
+    .setTimestamp()
+    .addField(
+      `${client.emojis.find(emoji => emoji.name == template.tank.icon)} ${
+        template.tank.title
+      }`,
+      template.tank.list
+    )
+    .addField(
+      `${client.emojis.find(emoji => emoji.name == template.healer.icon)} ${
+        template.healer.title
+      }`,
+      template.healer.list
+    )
+    .addField(
+      `${client.emojis.find(emoji => emoji.name == template.dps.icon)} ${
+        template.dps.title
+      }`,
+      template.dps.list
+    );
+};
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const login = async () => {
+  for (let tries = 0; ; tries++) {
+    try {
+      client.login(process.env.TOKEN).catch(err => console.log(err));
+      break;
+    } catch (err) {
+      if (tries < 10) {
+        console.log("Login failed, sleeping");
+        sleep(30000);
+      } else throw err;
+    }
+  }
+};
+
+login();
